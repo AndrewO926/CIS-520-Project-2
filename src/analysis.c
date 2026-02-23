1afbbffb952fbac0144893c4cce86a1d602575c7
AndrewO926 <andrewosterhaus@icloud.com>
2026-02-22 18:16:51 -0600

    Analysis and CMake changes

--- src/analysis.c
+++ src/analysis.c
@@ -14,13 +14,28 @@
 // THIS IS NOT FINISHED.
 int main(int argc, char **argv) 
 {
-	if (argc < 3) 
+	if (argc < 3)
 	{
-		printf("%s <pcb file> <schedule algorithm> [quantum]\n", argv[0]);
-		return EXIT_FAILURE;
+			printf("%s <pcb file> <schedule algorithm> [quantum]\n", argv[0]);
+			return EXIT_FAILURE;
 	}
 
-	abort();		// REPLACE ME with implementation.
+	char* file = argv[1];
+	char* algorithm = argv[2];
+
+	ScheduleResult_t times;
+
+	if(strcmp(algorithm, FCFS) == 0){
+			bool success = first_come_first_serve(load_process_control_blocks(file), &times);
+			if(!success){
+					printf("first come first serve failed idk why");
+					return EXIT_FAILURE;
+			}
+	}
+
+	printf("Average waiting time: %f\n", times.average_waiting_time);
+	printf("Average turnaround time: %f\n", times.average_waiting_time);
+	printf("Total run time: %ld\n", times.total_run_time);
 
 	return EXIT_SUCCESS;
 }
